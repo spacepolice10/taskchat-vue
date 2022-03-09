ba0dbc50931be1b0c0f675fbe3024e0c1c9a0f87
spacepolice10 <44314842+spacepolice10@users.noreply.github.com>
2022-03-09 15:25:16 +0300

    feat: add adding new tasks function

---
+++ src/components/NewTask.vue
@@ -0,0 +1,18 @@
+<script setup>
+    defineProps(['modelValue'])
+    defineEmits(['update:modelValue'])
+</script>
+
+<template>
+    <div class="bg-white">
+        <input 
+        :value="modelValue"
+        @input="$emit('update:modelValue', $event.target.value)"
+        type="text" />
+        <button
+        @click="$emit('createNewTask')"
+        >
+            Send
+        </button>
+    </div>
+</template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,10 +4,24 @@
   import {ref, onMounted} from 'vue'
 
   let database = ref([])
+  
+  //id of the list that will be used as a target for adding function (adding new elements to list manually)
+  let addingListId = ref(0)
+  //v-model for input that makes possible dynamically change the name of the task sent to list
+  let newTaskInput = ref('')
 
   async function loadDataFromDB () {
     const {data} = await supabase.from('lists').select('*, cards(*)')
     database.value = data
+  }
+  //manually create new task in supabase
+  async function createNewTask () {
+    const {data} = await supabase.from('cards').insert(
+      {
+        name: newTaskInput.value, 
+        list_id: addingListId.value
+      }
+    )
   }
 
   onMounted(async () => {
@@ -23,7 +37,13 @@
     :key="list.id"
     v-for="list in database"
     >
-    <button>+</button>
+    <button @click="addingListId = list.id">+</button>
+    <new-task 
+    v-if="list.id == addingListId"
+    v-model="newTaskInput"
+    @createNewTask="createNewTask"
+    >
+    </new-task>
       <div
       class="border p-4 m-2"
       :key="card.id"
