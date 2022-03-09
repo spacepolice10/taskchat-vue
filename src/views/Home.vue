1b360c990a3badb63a66e804f69ecf8752323775
spacepolice10 <44314842+spacepolice10@users.noreply.github.com>
2022-03-09 14:52:56 +0300

    feat: add connection to supabase

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,37 @@
 <script setup>
-import ButtonRepo from '@/components/ButtonRepo.vue'
+  import supabase from '../../lib/db'
+  import newTask from '@/components/NewTask.vue'
+  import {ref, onMounted} from 'vue'
+
+  let database = ref([])
+
+  async function loadDataFromDB () {
+    const {data} = await supabase.from('lists').select('*, cards(*)')
+    database.value = data
+  }
+
+  onMounted(async () => {
+    loadDataFromDB()
+  })
 
 </script>
 
 <template>
-  <div class="bg-gray-50">
-
+  <div class="flex justify-center bg-gray-50">
+    <div 
+    class="flex flex-col gap-6 border m-2"
+    :key="list.id"
+    v-for="list in database"
+    >
+    <button>+</button>
+      <div
+      class="border p-4 m-2"
+      :key="card.id"
+      v-for="card in list.cards.sort(function(a,b) {return a.position - b.position})"
+      >
+        {{card.name}}
+      </div>
+    </div>
   </div>
 </template>
   
