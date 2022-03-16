3a103f9292eef4c9f44caf15f41e17af02b17ca6
spacepolice10 <44314842+spacepolice10@users.noreply.github.com>
2022-03-16 15:33:56 +0300

    feat: change the process of fetching data

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,12 @@
 <script setup>
 import supabase from "../../lib/db";
-import newTask from "@/components/NewTask.vue";
-import Card from "@/components/Card.vue";
+import List from "@/components/List.vue";
 import { ref, onMounted } from "vue";
-import { Popover, PopoverButton, PopoverPanel } from "@headlessui/vue";
 
 let database = ref([]);
-//id of the list that will be used as a target for adding function (adding new elements to list manually)
-let addingListId = ref(0);
 
 async function loadDataFromDB() {
-  const { data } = await supabase.from("lists").select("*, cards(*)");
+  const { data } = await supabase.from("lists").select("*");
   database.value = data;
 }
 
@@ -20,40 +16,13 @@ onMounted(async () => {
 </script>
 
 <template>
-  <div class="flex max-h-screen w-full snap-x overflow-scroll bg-gray-50">
+  <div class="flex max-h-screen w-full snap-x overflow-scroll bg-slate-100">
     <div
-      class="max-w m-2 flex min-w-fit snap-center scroll-m-0 flex-col gap-6 overflow-y-scroll"
+      class="m-2 flex w-full min-w-fit snap-center scroll-m-0 flex-col gap-6 overflow-y-scroll"
       :key="list.id"
       v-for="list in database"
     >
-      <Popover class="relative flex items-center p-2">
-        {{ list.name }}
-        <PopoverButton
-          class="font-2xl m-2 flex h-6 w-10 items-center justify-center rounded-md bg-gray-200 p-2 text-gray-400 hover:bg-gray-300"
-          @click="addingListId = list.id"
-        >
-          +
-        </PopoverButton>
-        <transition
-          enter-active-class="transition duration-200 ease-out"
-          enter-from-class="translate-y-1 opacity-0"
-          enter-to-class="translate-y-0 opacity-100"
-          leave-active-class="transition duration-150 ease-in"
-          leave-from-class="translate-y-0 opacity-100"
-          leave-to-class="translate-y-1 opacity-0"
-        >
-          <PopoverPanel class="fixed z-10 mt-96">
-            <new-task :id="addingListId" />
-          </PopoverPanel>
-        </transition>
-      </Popover>
-      <card
-        :key="card.id"
-        v-for="card in list.cards.sort(function (a, b) {
-          return a.position - b.position;
-        })"
-        :card="card"
-      />
+      <list :list="list" />
     </div>
   </div>
 </template>
